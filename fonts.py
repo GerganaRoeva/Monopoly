f6a86e3b4ac00520698369a38eb554df43d7db50
GerganaRoeva <gergana.roeva@gmail.com>
2021-03-15 18:50:21 +0200

    monopoly

---
+++ fonts.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+# Дoбавих още фонтове
+corbel = pygame.font.SysFont('Corbel', 35)
+calibri = pygame.font.SysFont('Calibri', 35)
+ariel = pygame.font.SysFont('Ariel', 35)
+times_new_roman = pygame.font.SysFont('Times New Roman', 35)
+comic_sans = pygame.font.SysFont('Comic Sans', 35)
+
+small_corbel = pygame.font.SysFont('Corbel', 20)
+small_calibri = pygame.font.SysFont('Calibri', 20)
+small_ariel = pygame.font.SysFont('Ariel', 20)
+small_times_new_roman = pygame.font.SysFont('Times New Roman', 20)
+small_comic_sans = pygame.font.SysFont('Comic Sans', 20)
+
+big_corbel = pygame.font.SysFont('Corbel', 35)
+big_calibri = pygame.font.SysFont('Calibri', 35)
+big_ariel = pygame.font.SysFont('Ariel', 35)
+big_times_new_roman = pygame.font.SysFont('Times New Roman', 35)
+big_comic_sans = pygame.font.SysFont('Comic Sans', 35)
